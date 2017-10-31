271fbc39d24930261dbaf99718198c57f150eace
Brandon Patterson <brandnpatterson@gmail.com>
2017-10-31 10:14:59 -0500

    completed lesson 16

---
+++ src/components/Message.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="box">
+    <p>
+      <slot></slot>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+  .box {
+    background: whitesmoke;
+    padding: 10px;
+    margin: 40px;
+    border: 1px solid black;
+  }
+</style>
